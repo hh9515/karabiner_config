24ad57eed9a2157568af48c37571c4afaa43d49a
huhao7 <huhao7@xiaomi.com>
2023-11-07 15:39:23 +0800

    feat: format

---
+++ etc/capslock to rightshift alone to f18 in vscode and finder.js
@@ -0,0 +1,29 @@
+// {
+//     "description": "Change caps_lock to right_shift in finder and vscode (alone to f18)",
+//     "manipulators": [
+//         {
+//             "conditions": [
+//                 {
+//                     "bundle_identifiers": [
+//                         "^com.apple.finder|com.microsoft.VSCode$"
+//                     ],
+//                     "type": "frontmost_application_if"
+//                 }
+//             ],
+//             "from": {
+//                 "key_code": "caps_lock"
+//             },
+//             "to": [
+//                 {
+//                     "key_code": "right_shift"
+//                 }
+//             ],
+//             "to_if_alone": [
+//                 {
+//                     "key_code": "f18"
+//                 }
+//             ],
+//             "type": "basic"
+//         }
+//     ]
+// },
